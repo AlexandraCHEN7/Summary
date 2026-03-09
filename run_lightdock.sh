0cc74bca2fa561dd3a450f777075999743c50cb8
AlexandraCHEN7 <E1324262@u.nus.edu>
2026-03-09 20:37:18 +0800

    Update run_lightdock.sh

--- run_lightdock.sh
+++ run_lightdock.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=cpu
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --chdir=/spinning1/scratch/mc2571/model_outputs/test_case/5DJT
+#SBATCH --chdir=/spinning1/scratch/mc2571/model_outputs/test_case/1LP1
 
 eval "$(conda shell.bash hook)"
 conda activate lightdock311
@@ -16,7 +16,7 @@ lightdock3.py setup.json 50 -s fastdfire -c 4
 
 for s in swarm_*/; do
     cd "$s"
-    lgd_generate_conformations.py ../LOV2_A.pdb ../affibody_B.pdb gso_50.out 200
+    lgd_generate_conformations.py ../Z_A.pdb ../affibody_B.pdb gso_50.out 200
     lgd_cluster_bsas.py gso_50.out
     cd ..
 done
