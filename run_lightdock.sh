5b6e2ad9498e7565cfecd8109685a128fcc8b151
AlexandraCHEN7 <E1324262@u.nus.edu>
2026-03-09 20:41:10 +0800

    Update run_lightdock.sh

--- run_lightdock.sh
+++ run_lightdock.sh
@@ -11,16 +11,16 @@
 eval "$(conda shell.bash hook)"
 conda activate lightdock311
 
-lightdock3_setup.py LOV2_A.pdb affibody_B.pdb --noxt --noh --now
+lightdock3_setup.py Z_B.pdb affibody_A.pdb --noxt --noh --now
 lightdock3.py setup.json 50 -s fastdfire -c 4
 
 for s in swarm_*/; do
     cd "$s"
-    lgd_generate_conformations.py ../Z_A.pdb ../affibody_B.pdb gso_50.out 200
+    lgd_generate_conformations.py ../Z_B.pdb ../affibody_A.pdb gso_50.out 200
     lgd_cluster_bsas.py gso_50.out
     cd ..
 done
 
 lgd_rank.py 50 50
 
-lgd_top.py LOV2_A.pdb affibody_B.pdb rank_by_scoring.list 10
+lgd_top.py Z_B.pdb affibody_A.pdb rank_by_scoring.list 10
